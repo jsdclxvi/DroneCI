68a54f2f361bf207ea3f5bc96efdfcebf7d3caa5
Angga R <nggdclxvi@protonmail.com>
2021-06-03 20:55:10 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://$GITHUB_TOKEN@github.com/AnggaR96s/msm-4.14 kernel -b main --depth=5
+git clone https://$GITHUB_TOKEN@github.com/AnggaR96s/msm-4.14 kernel -b base --depth=5
 cd kernel
 curl https://raw.githubusercontent.com/ARCHSTNC/DroneCI/main/build.sh | bash
