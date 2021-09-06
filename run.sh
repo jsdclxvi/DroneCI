f6c77429bb22ffb87c7846704bfb8a0edfd7dc38
Angga R <angga@linuxmail.org>
2021-09-06 19:26:58 +0700

    switch to main branch

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/AnggaR96s/kernel_xiaomi_joyeuse-1 kernel -b eleven --depth=5
+git clone https://github.com/AnggaR96s/kernel_xiaomi_joyeuse-1 kernel -b main --depth=5
 cd kernel
 curl https://raw.githubusercontent.com/ARCHSTNC/DroneCI/main/build.sh | bash
