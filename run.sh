98624eb3af01de221f9d503bd5d16c3d65e47f9b
jsdclxvi <90479200+jsdclxvi@users.noreply.github.com>
2022-01-01 03:14:44 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/risawitama/kernel_xiaomi_onclite.git --branch lont-R2 --depth=2 kernel
-cd kernel
-curl https://raw.githubusercontent.com/jsdclxvi/DroneCI/silont/build.sh | bash
