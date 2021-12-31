9ccae8fd99601d69813956f07c18873154ce5bd6
jsdclxvi <90479200+jsdclxvi@users.noreply.github.com>
2022-01-01 03:12:59 +0700

    Create collect.sh

---
+++ collect.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /tmp/rom
+
+source build/envsetup.sh
+breakfast onclite
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 20G
+ccache -o compression=true
+ccache -z
+croot
+brunch onclite &
+sleep 90m
+kill %1
+ccache -s
