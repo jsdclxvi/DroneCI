f2a5d2d1af9fb6572e02bcb828505a3129c95163
jsdclxvi <90479200+jsdclxvi@users.noreply.github.com>
2022-01-01 03:16:03 +0700

    Create final.sh

---
+++ final.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /tmp/rom
+
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 20G
+ccache -o compression=true
+ccache -z
+
+source build/envsetup.sh
+breakfast onclite
+croot
+brunch onclite
